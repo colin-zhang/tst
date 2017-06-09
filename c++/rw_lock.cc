33b1eaa70860f7b99e15079ff74950fac995027b
colin-zhang <ingm@foxmail.com>
2017-06-08 23:26:07 -0400

    test

--- c++/rw_lock.cc
+++ c++/rw_lock.cc
@@ -105,9 +105,16 @@ static volatile int g_running = 0;
 static volatile int g_locking = 0;
 static volatile uint64_t g_ref = 0;
 
+static __thread int g_core_num = 0;
+
 std::map<uint64_t, std::string> g_map;
 
 uint64_t g_cnt = 0;
+
+bool test_cnt(uint64_t n)
+{
+   return n >= 10000 * 1000 * 5; 
+}
 
 uint64_t getCurrTV()
 {
@@ -144,8 +151,9 @@ int add_ref()
 {
     if (g_locking) {
         return 0; 
+    } else {
+        return __sync_add_and_fetch(&g_ref, 1);
     }
-    return __sync_add_and_fetch(&g_ref, 1);
 }
 
 void sub_ref()
@@ -158,8 +166,11 @@ void* load(void* arg)
     cpu_set_t mask;
     int core = (int)(*(int*)arg);
 
+    g_core_num = core;
+
     printf("core id=%d \n", core);
     uint64_t n = 0;
+    uint64_t n_add = 0;
     char buf[32] = {0};
 
     CPU_ZERO(&mask);
@@ -169,7 +180,7 @@ void* load(void* arg)
     }
 
     while (g_running) {
-        if (core == 1) {
+        if (core != 2) {
             //snprintf(buf, sizeof(buf), "n=%lu", n);
             //printf("%s\n", buf);
             //g_map[n] = std::string(buf);
@@ -184,19 +195,24 @@ void* load(void* arg)
                 if (it == g_map.end()) {
                     printf("%s\n", "can not find 100");
                 } else {
-                    if ( n % 999999 == 0) {
-                        printf("core%02d:%s, n = %lu\n", core, it->second.c_str(), n);
+                    if ( n % 9999 == 0) {
+                        printf("core%02d:%s, n = %lu\n", g_core_num, it->second.c_str(), n);
                     }
                 }
                 //pthread_rwlock_unlock(&g_lock);
                 //rwlock_read_unlock(&g_lock2);
                 sub_ref();
+		n_add++;
             }
             //pthread_rwlock_unlock(&g_lock);
 
+            n++;
         }
-        n++;
-        __sync_fetch_and_add(&g_cnt, 1);
+        if(test_cnt(__sync_add_and_fetch(&g_cnt, 1))) {
+              printf("n=%lu, n_add=%lu\n", n, n_add);
+              break;
+        } 
+	
     }
     pthread_exit(NULL);
     return NULL;
@@ -235,38 +251,40 @@ int main(int argc, char const *argv[])
     }
 
     while (1) {
-        printf("g_map size = %lu\n", g_map.size());
+        //printf("g_map size = %lu\n", g_map.size());
 	
         //rwlock_write_lock(&g_lock2);
         //pthread_rwlock_wrlock(&g_lock);
         g_locking = 1;
         if (g_locking) {
-            _mm_pause();
+            //_mm_pause();
 	    while ( !__sync_bool_compare_and_swap(&g_ref, 0, -1)) {
+                   //printf("&&&&&&&&&&  g_map size = %lu\n", g_map.size());
 	           _mm_pause();
             }
             g_map.clear();
-            set_map(1000);
+            set_map(200);
             g_ref = 0;
             g_locking = 0;
         }
         //rwlock_write_unlock(&g_lock2);
         //pthread_rwlock_unlock(&g_lock);
-        printf("g_map size = %lu\n", g_map.size());
-        sleep(5);
-	if (g_cnt > 10000 * 10000 * 1) {
+        //printf("g_map size = %lu\n", g_map.size());
+        sleep(1);
+	if (test_cnt(g_cnt)) {
 		g_running = 0;
 		break;
 	}
     }
     b = getCurrTV();
-    printf("b - a = %lu\n", b - a);
 
     for (i = 0; i < num; i++) {
         if (thread_info[i].run) {
             pthread_join(thread_info[i].tid, NULL);
         }
     }
+
+    printf("b - a = %lu, g_cnt = %lu\n", b - a, g_cnt);
 
     free(thread_info);
     return 0;
