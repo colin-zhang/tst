a64f1356460a73f5d492e260a943ae7a84e7e325
colin-zhang <ingm@foxmail.com>
2017-06-11 16:20:15 +0800

    xx

--- c++/rw_lock.cc
+++ c++/rw_lock.cc
@@ -13,8 +13,9 @@
 #include <vector>
 #include <string>
 
-typedef struct {
+typedef struct rw_lock {
     volatile int32_t cnt; /**< -1 when W lock held, > 0 when R locks held. */
+    rw_lock() : cnt(0) {};
 } rwlock_t;
 
 #define RWLOCK_INITIALIZER { 0 }
@@ -101,9 +102,12 @@ static rwlock_t g_lock2;
 static pthread_rwlock_t g_lock; 
 
 static volatile int g_running = 0;
+static volatile int g_locking = 0;
+static volatile uint64_t g_ref = 0;
 
 std::map<uint64_t, std::string> g_map;
 
+uint64_t g_cnt = 0;
 
 uint64_t getCurrTV()
 {
@@ -136,6 +140,19 @@ void set_map(int x)
 
 }
 
+int add_ref()
+{
+    if (g_locking) {
+        return 0; 
+    }
+    return __sync_add_and_fetch(&g_ref, 1);
+}
+
+void sub_ref()
+{
+   __sync_fetch_and_sub(&g_ref, 1); 
+}
+
 void* load(void* arg)
 {
     cpu_set_t mask;
@@ -157,9 +174,10 @@ void* load(void* arg)
             //printf("%s\n", buf);
             //g_map[n] = std::string(buf);
         } else {
-            if (rwlock_read_trylock(&g_lock2)) 
+            //if (rwlock_read_trylock(&g_lock2)) 
             //pthread_rwlock_rdlock(&g_lock);
-            //if (pthread_rwlock_tryrdlock(&g_lock))
+            //if (!pthread_rwlock_tryrdlock(&g_lock))
+            if (add_ref())
             {
                 //rwlock_read_lock(&g_lock2);
                 auto it = g_map.find(100);
@@ -167,16 +185,18 @@ void* load(void* arg)
                     printf("%s\n", "can not find 100");
                 } else {
                     if ( n % 999999 == 0) {
-                        printf("%s\n", it->second.c_str());
+                        printf("core%02d:%s, n = %lu\n", core, it->second.c_str(), n);
                     }
                 }
                 //pthread_rwlock_unlock(&g_lock);
-                rwlock_read_unlock(&g_lock2);
+                //rwlock_read_unlock(&g_lock2);
+                sub_ref();
             }
             //pthread_rwlock_unlock(&g_lock);
 
         }
         n++;
+        __sync_fetch_and_add(&g_cnt, 1);
     }
     pthread_exit(NULL);
     return NULL;
@@ -190,9 +210,11 @@ int main(int argc, char const *argv[])
     struct ThreadInfo* thread_info = NULL;
     float rate;
 
+    uint64_t a , b;
+
     g_running = 1;
-    //set_map(1000);
-    rwlock_init(&g_lock2);
+    set_map(1000);
+    //rwlock_init(&g_lock2);
     pthread_rwlock_init(&g_lock, NULL);
 
     thread_info = (struct ThreadInfo*)calloc(sizeof(struct ThreadInfo), num);
@@ -201,7 +223,7 @@ int main(int argc, char const *argv[])
         exit(-1);
     }
 
-
+    a = getCurrTV(); 
     for (i = 0; i < num; i++) {
         thread_info[i].core_num = i;
         thread_info[i].run = 0;
@@ -214,15 +236,31 @@ int main(int argc, char const *argv[])
 
     while (1) {
         printf("g_map size = %lu\n", g_map.size());
-        rwlock_write_lock(&g_lock2);
+	
+        //rwlock_write_lock(&g_lock2);
         //pthread_rwlock_wrlock(&g_lock);
-        g_map.clear();
-        set_map(1000);
-        rwlock_write_unlock(&g_lock2);
+        g_locking = 1;
+        if (g_locking) {
+            _mm_pause();
+	    while ( !__sync_bool_compare_and_swap(&g_ref, 0, -1)) {
+	           _mm_pause();
+            }
+            g_map.clear();
+            set_map(1000);
+            g_ref = 0;
+            g_locking = 0;
+        }
+        //rwlock_write_unlock(&g_lock2);
         //pthread_rwlock_unlock(&g_lock);
         printf("g_map size = %lu\n", g_map.size());
-        sleep(2);
+        sleep(5);
+	if (g_cnt > 10000 * 10000 * 1) {
+		g_running = 0;
+		break;
+	}
     }
+    b = getCurrTV();
+    printf("b - a = %lu\n", b - a);
 
     for (i = 0; i < num; i++) {
         if (thread_info[i].run) {
